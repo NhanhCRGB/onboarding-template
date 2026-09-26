c717c8620fe8da707faece581efc9bd3ebb30434
NhanhCRGB <nhatanhzingcad@gmail.com>
2026-09-25 23:21:49 -0400

    Maybe not

--- src/submission.hpp
+++ src/submission.hpp
@@ -81,7 +81,7 @@ for (size_t i = 1; i < r - 1; i++){
   const double* __restrict U_row = old_data + (i + 1) * s_old;
   double* __restrict new_data_row = new_data +  i * s_new;
 
-#pragma omp simd
+#pragma omp simd simdlen(4)
 for (size_t j  = 1;  j < c - 1; j++){
   new_data_row[j] = 0.5 * C_row[j] + 0.125 * (L_row[j]  + U_row[j] + C_row[j-1] + C_row[j+1]);
     }
